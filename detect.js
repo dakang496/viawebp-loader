44e58953b05e4e15343356a83bf1acd51eccf3e0
dakang496 <1208303205@qq.com>
2017-09-11 18:22:03 +0800

    添加浏览器检测是否支持webp

---
+++ detect.js
@@ -0,0 +1,38 @@
+ /**
+  *  inspired by 
+  *  https://github.com/scottjehl/picturefill
+  *  https://developers.google.com/speed/webp/faq#which_web_browsers_natively_support_webp
+  */
+ function checkSupport(callback) {
+   var img = new Image();
+   img.onload = function () {
+     var result = img.width === 1;
+     callback(result);
+   };
+   img.onerror = function () {
+     callback(false);
+   }
+   img.src = 'data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAABBxAR/Q9ERP8DAABWUDggGAAAADABAJ0BKgEAAQADADQlpAADcAD++/1QAA==';
+ }
+
+ var detect = {
+   isSupport: function () {
+     if (typeof window === 'undefined') {
+       return false;
+     }
+     //  if (typeof module !== 'undefined' && module.exports) {
+     //    return false;
+     //  }
+     checkSupport(function (supported) {
+       detect.isSupport = function () {
+         return supported;
+       }
+     });
+   }
+ }
+
+ if (typeof window !== 'undefined') {
+   detect.isSupport();
+ }
+
+ module.exports = detect;
--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-var imageLoaderPath = require.resolve('./image.js');
+var imageScriptPath = require.resolve('./image.js');
+var detectScriptPath = require.resolve('./detect.js');
 var loaderUtils = require("loader-utils");
 
 module.exports = function (content) {
-     this.cacheable && this.cacheable();
-    var regx = /module\.exports\s*?=/;
-    var existFile = regx.test(content);
+  this.cacheable && this.cacheable();
+  var regx = /module\.exports\s*?=/;
+  var existFile = regx.test(content);
 
-    console.log(content);
+  var urlQuery = this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null;
+  var query = Object.assign({}, loaderUtils.getOptions(this), urlQuery);
 
-    var query = loaderUtils.getOptions(this);
-    var imageLoaderOptions = query ? ('?' + JSON.stringify(query)) : '';
+  var imageLoaderOptions = '?' + JSON.stringify(query);
+  var webpExportValue = 'require(' + JSON.stringify('-!' + imageScriptPath + imageLoaderOptions + '!' + this.resource) + ');'
 
-    var file = this.resource;
-    var webpExportValue = 'require(' + JSON.stringify('-!' + imageLoaderPath + imageLoaderOptions + '!' + file) + ');'
-
-    if (existFile) {
-        if (query.expose === 'webp') {
-            return 'module.exports=' + webpExportValue;
-        } else if (query.expose === 'all') {
-            var webpExportText = 'module.exports.webp=' + webpExportValue;
-            var defaultExportText = content.replace(regx, 'module.exports.default=');
-            return webpExportText + defaultExportText;
-        } else {
-            return webpExportValue + content;
+  var detectOptions = query.detect;
+  if (detectOptions && detectOptions.enable) {
+    var defaultExportText = content.replace(regx, 'module.exports.default=');
+    var deleteCache = `delete require.cache[require.resolve('${this.resource}')]; `;
+    if (detectOptions.auto) {
+      var scriptPath = detectOptions.script || detectScriptPath;
+      return deleteCache + defaultExportText + `
+        var detect= require('${scriptPath}');
+        var webpPath=${webpExportValue}
+        var imageInfo=module.exports.default;
+        if(detect.isSupport()){
+          imageInfo=webpPath;
         }
+        module.exports=imageInfo;
+      `;
     } else {
-        return 'module.exports=' + webpExportValue;
+      var variable = variable || '__webp_support__'
+      return (detectOptions.cache ? deleteCache : '') + defaultExportText + `
+        var webpPath=${webpExportValue}
+        var imageInfo=module.exports.default;
+        if(window.${variable}){
+          imageInfo=webpPath;
+        }
+        module.exports=imageInfo;
+      `;
     }
+  }
+  if (existFile) {
+    if (query.expose === 'webp') {
+      return 'module.exports=' + webpExportValue;
+    } else if (query.expose === 'all') {
+      var webpExportText = 'module.exports.webp=' + webpExportValue;
+      var defaultExportText = content.replace(regx, 'module.exports.default=');
+      return webpExportText + defaultExportText;
+    } else {
+      return webpExportValue + content;
+    }
+  } else {
+    return 'module.exports=' + webpExportValue;
+  }
 }
