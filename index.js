d9bc97f2d7b6e06f9e722ff61b459a2ebddc0d06
dakang496 <1208303205@qq.com>
2017-09-12 15:27:37 +0800

    url可用参数覆盖配置

--- index.js
+++ index.js
@@ -1,67 +1,30 @@
-var imageScriptPath = require.resolve('./image.js');
-var detectScriptPath = require.resolve('./detect.js');
+var imageLoaderPath = require.resolve('./image.js');
 var loaderUtils = require("loader-utils");
 
 module.exports = function (content) {
-  this.cacheable && this.cacheable();
-  var regx = /module\.exports\s*?=/;
-  var existFile = regx.test(content);
+     this.cacheable && this.cacheable();
+    var regx = /module\.exports\s*?=/;
+    var existFile = regx.test(content);
 
-  var urlQuery = this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null;
-  var query = Object.assign({}, loaderUtils.getOptions(this), urlQuery);
+    var urlQuery = this.resourceQuery ? loaderUtils.parseQuery(this.resourceQuery) : null;
+    var query = Object.assign({}, loaderUtils.getOptions(this), urlQuery);
+  
+    var imageLoaderOptions = '?' + JSON.stringify(query);
 
-  var imageLoaderOptions = '?' + JSON.stringify(query);
-  var webpExportValue = 'require(' + JSON.stringify('-!' + imageScriptPath + imageLoaderOptions + '!' + this.resource) + ');'
+    var file = this.resource;
+    var webpExportValue = 'require(' + JSON.stringify('-!' + imageLoaderPath + imageLoaderOptions + '!' + file) + ');'
 
-  var detectOptions = query.detect;
-  if (detectOptions && detectOptions.enable) {
-    var defaultExportText = content.replace(regx, 'module.exports.default=');
-    var deleteCache = `delete require.cache[require.resolve('${this.resource}')]; `;
-    if (detectOptions.auto) {
-      var scriptPath = detectOptions.script || detectScriptPath;
-      return defaultExportText + `
-        var detect= require('${scriptPath}');
-        var webpPath=${webpExportValue}
-        var imageInfo=module.exports.default;
-        var support=detect.isSupport();
-        if(support){
-          imageInfo=webpPath;
-        }else if(support===0){
-          ${deleteCache} 
+    if (existFile) {
+        if (query.expose === 'webp') {
+            return 'module.exports=' + webpExportValue;
+        } else if (query.expose === 'all') {
+            var webpExportText = 'module.exports.webp=' + webpExportValue;
+            var defaultExportText = content.replace(regx, 'module.exports.default=');
+            return webpExportText + defaultExportText;
+        } else {
+            return webpExportValue + content;
         }
-        module.exports=imageInfo;
-      `;
     } else {
-      var serverSupportText = detectOptions.serverVariable ? detectOptions.serverVariable : 'global.__webp_support__';
-      var clientSupportText = detectOptions.clientVariable ? detectOptions.clientVariable : 'window.__webp_support__';
-
-      return (detectOptions.cache ? deleteCache : '') + defaultExportText + `
-        var webpPath=${webpExportValue}
-        var imageInfo=module.exports.default;
-        var support=false;
-        if(typeof window === 'undefined'){//node
-          support=${serverSupportText}
-        }else{//window
-          support=${clientSupportText}
-        }
-        if(support){
-           imageInfo=webpPath;
-        }
-        module.exports=imageInfo;
-      `;
+        return 'module.exports=' + webpExportValue;
     }
-  }
-  if (existFile) {
-    if (query.expose === 'webp') {
-      return 'module.exports=' + webpExportValue;
-    } else if (query.expose === 'all') {
-      var webpExportText = 'module.exports.webp=' + webpExportValue;
-      var defaultExportText = content.replace(regx, 'module.exports.default=');
-      return webpExportText + defaultExportText;
-    } else {
-      return webpExportValue + content;
-    }
-  } else {
-    return 'module.exports=' + webpExportValue;
-  }
 }
